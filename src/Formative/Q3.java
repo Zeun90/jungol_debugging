b462aad0d94a49ff2a4e147fb254b964fb8c44c1
Lee JiEun <gsilver190@gmail.com>
2024-06-16 16:22:28 +0900

    형성 평가 3

---
+++ src/Formative/Q3.java
@@ -0,0 +1,10 @@
+package Formative;
+
+public class Q3 {
+    public static void main(String[] args) {
+        int a = 5; // a => 5
+        a += 10; // a => 5 -> 15
+        a = a -1; // a => 15 -> 14
+        System.out.printf("%d\n", 15);
+    }
+}
